23beaaced960b83880d1d57aef7e6ff85971b1e1
Ingo Fruend <github@ingofruend.net>
2023-07-27 22:36:53 +0200

    feat(config): Add style as valid type

--- src/semv/config.py
+++ src/semv/config.py
@@ -2,6 +2,6 @@ from .types import InvalidCommitAction
 
 commit_types_minor = {'feat'}
 commit_types_patch = {'fix', 'perf'}
-commit_types_skip = {'chore', 'test', 'docs', 'ci', 'refactor'}
+commit_types_skip = {'chore', 'test', 'docs', 'ci', 'refactor', 'style'}
 
 invalid_commit_action = InvalidCommitAction.warning
