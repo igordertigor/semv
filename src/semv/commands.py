90590b93b0c5c0ad03f63fe7e7aa920e6351f168
Ingo Fruend <git@ingofruend.net>
2023-08-01 21:41:06 +0200

    refactor(commands): Cleaner separation

--- src/semv/commands.py
+++ src/semv/commands.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-import sys
 from .increment import DefaultIncrementer
 from .parse import AngularCommitParser
 from .version_control_system import Git
@@ -8,23 +6,19 @@ from . import errors
 from .types import Version, VersionIncrement
 
 
-def version_string() -> Version:
+def list_types(config: Config) -> str:
+    return config.format_types()
+
+
+def version_string(config: Config) -> Version:
     """Generate a version string for the next version
 
     Exceptions:
         NoNewVersion
         InvalidCommitType
+        InvalidCommitFormat
+        SuspiciousVersionIncrement
     """
-    pyproject = Path('pyproject.toml')
-    if pyproject.exists():
-        config = Config.parse(pyproject.read_text())
-    else:
-        config = Config()
-
-    if len(sys.argv) > 1 and sys.argv[1] == '--list-types':
-        print(config.format_types())
-        sys.exit(0)
-
     vcs = Git()
     cp = AngularCommitParser(config.invalid_commit_action)
     vi = DefaultIncrementer(
@@ -34,19 +28,12 @@ def version_string() -> Version:
         config.invalid_commit_action,
     )
 
-    try:
-        current_version = vcs.get_current_version()
-        commits_or_none = (
-            cp.parse(c) for c in vcs.get_commits_without(current_version)
-        )
-        commits = (c for c in commits_or_none if c is not None)
-        inc = vi.get_version_increment(commits)
-        if inc == VersionIncrement.skip:
-            raise errors.NoNewVersion
-        return current_version + inc
-    except errors.InvalidCommitType as e:
-        sys.stderr.write(f'ERROR: {e.args[0]}\n')
-        sys.exit(2)
-    except errors.InvalidCommitFormat as e:
-        sys.stderr.write(f'ERROR: {e.args[0]}\n')
-        sys.exit(2)
+    current_version = vcs.get_current_version()
+    commits_or_none = (
+        cp.parse(c) for c in vcs.get_commits_without(current_version)
+    )
+    commits = (c for c in commits_or_none if c is not None)
+    inc = vi.get_version_increment(commits)
+    if inc == VersionIncrement.skip:
+        raise errors.NoNewVersion
+    return current_version + inc
